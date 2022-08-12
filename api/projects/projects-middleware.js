7e1ce6b18737b42a751ffd93eaea11b3da5277fb
Matt Decker <matt@Matts-MBP.lan>
2022-08-12 16:32:14 -0400

    delete works

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -5,15 +5,34 @@ function validateProjectId(req, res, next) {
     Project.get(req.params.id)
         .then(result => {
             if (result == null) {
-                next({ status: 404, message: 'project not found'})
+                next({ status: 404, message: 'project not found'});
             } else {
                 req.existingProject = result;
                 next();
             }
         })
-        .catch(err => next(err))
+        .catch(err => next(err));
+}
+
+function validateProject(req, res, next) {
+    if (req.body.name == null || req.body.name === "") {
+        next({ status: 400, message: 'missing name' });
+    } else if (req.body.description == null || req.body.description === "") {
+        next({ status: 400, message: 'missing description' });
+    } else if (req.body.completed == null) {
+        next({ status: 400, message: 'must show completed' });
+    } else {
+        req.newProject = {
+            id: req.body.id,
+            name: req.body.name,
+            description: req.body.description,
+            completed: req.body.completed
+        }
+        next();
+    }
 }
 
 module.exports = {
     validateProjectId,
+    validateProject
 }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -3,25 +3,54 @@ const express = require('express');
 
 const Project = require('./projects-model.js');
 
-const { validateProjectId } = require('./projects-middleware')
+const { validateProjectId, validateProject } = require('./projects-middleware');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     Project.get()
         .then(projects => {
-            res.json(projects)
+            res.json(projects);
         })
-        .catch(err => next(err))
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: 'Error retrieving projects'})
+          })
 })
 
 router.get('/:id', validateProjectId, (req, res) => {
     Project.get(req.params.id)
         .then(project => {
-            res.json(req.existingProject)
+            res.json(req.existingProject);
         })
 })
 
+router.post('/', validateProject, (req, res) => {
+    Project.insert(req.newProject)
+        .then(project => {
+            res.status(201).json(project);
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: 'Error adding project'})
+          })
+})
+
+router.put('/:id', validateProject, validateProjectId, (req, res) => {
+    Project.update(req.params.id, req.newProject)
+        .then(project => {
+            res.json(project)
+        })
+})
+
+router.delete('/:id', validateProjectId, (req, res) => {
+    Project.remove(req.params.id)
+        .then(result => {
+            res.json(result)
+        })
+})
+
+// router.get()
 
 
 module.exports = router;
